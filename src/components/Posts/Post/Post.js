e69fb7a95df01307b133c0f2a26f517a14ea0f07
Nico Abrasaldo <nicoiabrasaldo@yahoo.com>
2020-12-17 15:52:42 +0800

    Updates

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,9 +1,61 @@
 import React from "react";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import moment from "moment";
 
 import useStyles from "./styles";
 const Post = () => {
   const classes = useStyles();
-  return <h1>Post</h1>;
+  return (
+    <Card className={classes.media}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+      <div className={classes.overlay2}>
+        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
+          <MoreHorizIcon fontSize="default" />
+        </Button>
+      </div>
+      <div className={classes.details}>
+        <Typgraphy variant="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typgraphy>
+      </div>
+      <CardContent>
+        <Typgraphy className={classes.title} variant="h5" gutterBottom>
+          {post.message}
+        </Typgraphy>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <ThumbUpAltIcon fontSize="small" />
+          Like
+          {post.likeCount}
+        </Button>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <DeleteIcon fontSize="small" />
+          Delete
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default Post;
